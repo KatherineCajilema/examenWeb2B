ada3372f078ca6d1d9328ea1aa3a20e2359b9b9a
KatherineCajilema <kate95kc@hotmail.com>
2018-07-16 20:53:13 -0500

    logeo

--- backendKat/src/app.module.ts
+++ backendKat/src/app.module.ts
@@ -20,7 +20,7 @@ import { UsuarioController } from './usuario/usuario.controller';
       port: 3306,
       username: 'root',
       password: '',
-      database: 'examenweb2bim',
+      database: 'prueba',
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
       options: { encrypt: true },
